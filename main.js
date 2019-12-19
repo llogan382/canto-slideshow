8755b29a4c842a75242ea2296239d9ba0367a1c7
Luke Logan <llogan382@gmail.com>
2019-12-19 11:47:13 -0500

    Change from HTML creation to Dom creation

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import CantoCreds from './keys.js';
 
 let appendHere = document.querySelector(".images");
-
+let cantoButton = document.createElement("li");
 
 /*
 
@@ -16,8 +16,8 @@ OwnerName: The Username of the asset owner.
 */
 
 let results = '/?sortBy=name&scheme=folder&layer=1';
-
-let request = new Request('https://highpoint.canto.com/api/v1/tree' + results, { method: 'GET', credentials: 'include' });
+let queryFolder = '/tree';
+let request = new Request('https://highpoint.canto.com/api/v1' + queryFolder + results, { method: 'GET', credentials: 'include' });
 const url = request.url;
 const method = request.method;
 const myHeaders = url.headers;
@@ -25,13 +25,27 @@ let headers = new Headers();
 headers.append("Authorization", "Bearer" + CantoCreds.accessToken);
 const credentials = request.credentials;
 
+
+const newQuery = (e) => {
+    e.preventDefault();
+    console.log('hello');
+}
+
 fetch(request)
     .then(response => response.json())
     .then(folders => {
+
+
         for (const folder of folders.results) {
+            //When you click the button, the query is updated to the next folder
+
+
+            // console.log(folder);
             var folderButtons =
                 appendHere.innerHTML += `
-            <a href="${folder.url.detail}"> ${folder.name}</a> </br>
-            `
+            <button onclick="newQuery" href="${folder} target="_blank"> ${folder.name}</button> </br>
+            `;
         }
+
     });
+
