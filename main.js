6c586124c788d35724b8553205a6bd0915e99bc8
Luke Logan <llogan382@gmail.com>
2019-12-19 13:10:39 -0500

    Gets all gallery lists

--- main.js
+++ main.js
@@ -1,51 +1,52 @@
 import CantoCreds from './keys.js';
 
-let appendHere = document.querySelector(".images");
-let cantoButton = document.createElement("li");
-
-/*
-
-The following properties are used in Canto:
-
-Scheme: Image, Video, Audio, Document, Presentation, and Other.
-Id: Id of the Folder, Album or Asset
-Created:  Original created time.
-Time: Last modified time.
-Owner: The User Id of first the uploader.
-OwnerName: The Username of the asset owner.
-*/
-
-let results = '/?sortBy=name&scheme=folder&layer=1';
-let queryFolder = '/tree';
-let request = new Request('https://highpoint.canto.com/api/v1' + queryFolder + results, { method: 'GET', credentials: 'include' });
-const url = request.url;
-const method = request.method;
-const myHeaders = url.headers;
-let headers = new Headers();
-headers.append("Authorization", "Bearer" + CantoCreds.accessToken);
-const credentials = request.credentials;
+let appendHere = document.querySelector(".searchResults");
 
 
-const newQuery = (e) => {
-    e.preventDefault();
-    console.log('hello');
-}
-
-fetch(request)
-    .then(response => response.json())
-    .then(folders => {
 
 
-        for (const folder of folders.results) {
-            //When you click the button, the query is updated to the next folder
+
+const runQuery = (directory) => {
+    appendHere.innerHTML = ""; //clear html before query
+
+    let results = '/?sortBy=name&scheme=folder&layer=1'; //how to sort the results
+
+    let queryFolder = (directory) ? directory : '/tree'; //default is tree. Then, run folders
+
+    let request = new Request('https://highpoint.canto.com/api/v1' + queryFolder + results, { method: 'GET', credentials: 'include' });
+
+    let headers = new Headers();
+    headers.append("Authorization", "Bearer" + CantoCreds.accessToken);
+
+    fetch(request)
+        .then(response => response.json())
+        .then(folders => {
 
 
-            // console.log(folder);
-            var folderButtons =
-                appendHere.innerHTML += `
-            <button onclick="newQuery" href="${folder} target="_blank"> ${folder.name}</button> </br>
-            `;
-        }
+            for (const folder of folders.results) { //loop over results
 
-    });
+                var cantoButton = document.createElement("A"); //create a buton
 
+                cantoButton.setAttribute("href", folder.url.detail); //create link for each button
+
+                var textnode = document.createTextNode(folder.name); //add text to the button
+
+                cantoButton.appendChild(textnode);
+
+
+                appendHere.appendChild(cantoButton).addEventListener("click", function newQuery(e) { //add an event listener
+
+                    event.preventDefault();
+
+                    queryFolder = '/folder/' + folder.id; //update query to look in folders
+
+                    runQuery(queryFolder); //pass the new query in to run the next search
+                });
+
+                cantoButton.insertAdjacentHTML("afterend", "</br>"); //add a break after each button for the list
+            }
+
+        });
+};
+
+runQuery();
